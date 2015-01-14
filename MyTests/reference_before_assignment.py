9cb2ca9c757b4937fc8cb2f7a8fa5633607b60d7
Victor Yacovlev <v.yacovlev@gmail.com>
2015-01-14 22:18:17 +0300

    New tests

---
+++ MyTests/reference_before_assignment.py
@@ -0,0 +1,6 @@
+def main():
+    print(r)
+    r = 123
+
+if __name__ == "__main__":
+    main()
---
+++ MyTests/test6.py
@@ -0,0 +1,11 @@
+﻿a = 1
+def f():
+    
+    a = 1; a = a + 1
+    a = 3
+    for i in range(0, 10):
+        print("")
+    
+    
+    
+f()
